7d724723a43f94ecd179522b30c2e25a29bd0666
Rahma Henchi <rahmahenchi@gmail.com>
2023-02-22 14:39:20 +0100

    Add event and user models

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+const UserSchema = Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User
