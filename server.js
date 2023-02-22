2c66bcdcecc618caa87ec7a2cf2270bdcab9eee6
Rahma Henchi <rahmahenchi@gmail.com>
2023-02-22 14:24:07 +0100

    Set up connection with data base

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
 app.use((req, res) => {
     res.json({ message: "Hello from Express" });
 })
+
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.DB_CONNECTION)
+        .then(() => {
+            console.log('Successfully connected to MongoDB');
+            app.listen(PORT, () => {
+                console.log(`Server is listening on port ${PORT}`);
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        })
